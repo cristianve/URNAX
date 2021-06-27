be24ca0108cdeeb75f32e243f9809cda21b8e93f
Cristian Vega <97cvega@gmail.com>
2021-06-27 23:30:14 +0200

    comment code

--- src/main/scala/Domain/Direction.scala
+++ src/main/scala/Domain/Direction.scala
@@ -2,7 +2,7 @@ package com.redgroup.votox
 package Domain
 
 /**
- * Case class ya que nos permite tener parametros inmutables y sintaxis de inicialización compacta.
+ * Case class ya que nos permite tener parametros inmutables y sintaxis de inicialización compacta (Minimal class).
  *
  * @param city nombre de la ciudad
  * @param state nombre de la provincia
